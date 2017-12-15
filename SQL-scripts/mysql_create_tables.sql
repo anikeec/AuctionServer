40d94479f9ab123f5a65dfdc1049ecf691046142
apu <pasha_anik@ukr.net>
2017-12-15 17:01:33 +0200

    Add scripts for database

---
+++ SQL-scripts/mysql_create_tables.sql
@@ -0,0 +1,69 @@
+use auctiondb;
+
+/* DROP TABLE USTATUS; */
+
+CREATE TABLE USTATUS(
+ status_id  INT NOT NULL,
+ name VARCHAR(20),
+ PRIMARY KEY (status_id)
+);
+
+/* DROP TABLE USER; */
+
+CREATE TABLE USER(
+ user_id  INT NOT NULL,
+ login VARCHAR(20),
+ passw_hash VARCHAR(20),
+ status_id INT,
+ used BOOLEAN DEFAULT TRUE,
+ PRIMARY KEY (user_id),
+ FOREIGN KEY (status_id) REFERENCES USTATUS (status_id)
+);
+
+/* DROP TABLE LOTSTATUS; */
+
+CREATE TABLE LOTSTATUS(
+ status_id  INT NOT NULL,
+ name VARCHAR(20),
+ PRIMARY KEY (status_id)
+);
+
+/* DROP TABLE LOT; */
+
+CREATE TABLE LOT(
+ lot_id  INT NOT NULL,
+ name VARCHAR(30),
+ start_price INT,
+ start_date DATE,
+ finish_date DATE,
+ last_rate INT,
+ last_rate_user_id INT,
+ status_id INT,
+ PRIMARY KEY (lot_id),
+ FOREIGN KEY (status_id) REFERENCES LOTSTATUS (status_id),
+ FOREIGN KEY (last_rate_user_id) REFERENCES USER (user_id)
+);
+
+/* DROP TABLE OBSERVES; */
+
+CREATE TABLE OBSERVES(
+ id  INT NOT NULL,
+ user_id INT,
+ lot_id INT,
+ PRIMARY KEY (id),
+ FOREIGN KEY (user_id) REFERENCES USER (user_id),
+ FOREIGN KEY (lot_id) REFERENCES LOT (lot_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SQL-scripts/mysql_createdb.sql
@@ -0,0 +1,5 @@
+/* DROP DATABASE auction; */
+
+CREATE DATABASE auctiondb
+    CHARACTER SET utf8
+    COLLATE utf8_general_ci;
