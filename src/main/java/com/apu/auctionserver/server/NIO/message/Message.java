e52d34f2adfde7b4801116f6449efc606d1c55e7
Pavel Anikeychik <pasha_anik@ukr.net>
2017-12-24 20:28:59 +0200

    Remake internal logic for Message, but I have to remake last method

--- src/main/java/com/apu/auctionserver/server/NIO/message/Message.java
+++ src/main/java/com/apu/auctionserver/server/NIO/message/Message.java
@@ -16,7 +16,8 @@ import java.util.List;
  */
 public class Message {
     
-    private StringBuffer stringBuffer = new StringBuffer();
+//    private StringBuffer stringBuffer = new StringBuffer();
+    private byte[] byteArray;
     
     public long socketId = 0; // the id of source socket or destination socket, depending on whether is going in or out.
     
@@ -24,34 +25,45 @@ public class Message {
     }
     
     public byte[] getMessageAsArray() {        
-        byte[] array = stringBuffer.toString().getBytes();
-        return array;
+//        byte[] array = stringBuffer.toString().getBytes();
+//        return array;
+        if(this.byteArray == null)
+            return new byte[0];
+        else
+            return this.byteArray;
     }
     
     public String getMessageStr() {       
-        return stringBuffer.toString();
+//        return stringBuffer.toString();
+        if(this.byteArray == null)
+            return "";
+        else
+            return new String(this.byteArray); 
     }
 
     public void writeToMessage(byte[] byteArray){
-        String str = new String(byteArray);
-        this.stringBuffer = new StringBuffer(str);
+//        String str = new String(byteArray);
+//        this.stringBuffer = new StringBuffer(str);
+        this.byteArray = byteArray;
     }
 
-    public void writePartialMessageToMessage(ByteBuffer srcBuffer, int endIndex){
-        int size = ServerSocketNIOProcessor.BYTE_BUFFER_SIZE;
+    public void writePartialMessageToMessage(ByteBuffer srcBuffer, int endIndex) {        
+//        int size = ServerSocketNIOProcessor.BYTE_BUFFER_SIZE;
         int length = endIndex + 1;                                              //question ????????
-//        int addSymbolsLength = 2;
-        byte[] destBuffer = new byte[length];
-        System.arraycopy(srcBuffer.array(), 0, destBuffer, 0, length);
-        String str = new String(destBuffer);
-//        destBuffer = new byte[size];
-        this.stringBuffer.append(str);
-        
-//        System.arraycopy(srcBuffer.array(), 
-//                            length, 
-//                            destBuffer, 
-//                            0, 
-//                            size - length);
+        this.byteArray = new byte[length];
+        System.arraycopy(srcBuffer.array(), 0, this.byteArray, 0, length);
+////        int addSymbolsLength = 2;
+//        byte[] destBuffer = new byte[length];
+//        System.arraycopy(srcBuffer.array(), 0, destBuffer, 0, length);
+//        String str = new String(destBuffer);
+////        destBuffer = new byte[size];
+//        this.stringBuffer.append(str);
+//        
+////        System.arraycopy(srcBuffer.array(), 
+////                            length, 
+////                            destBuffer, 
+////                            0, 
+////                            size - length);
         srcBuffer.clear();
 //        srcBuffer.put(destBuffer, 0, size);
     }
